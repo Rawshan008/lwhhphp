91b07d48b6dd851ed02ecd9233f47c3590bc454c
Rawshan008 <pust.love008@gmail.com>
2018-10-01 11:42:50 +0600

    Leap year counting by if else

--- index.php
+++ index.php
@@ -1,2 +1,20 @@
 <?php 
-echo "Learning PHP is start with hasin hyder";
+$year = 2018;
+
+if($year % 4 == 0 && $year % 100 == 0 && $year % 400 ==0){
+    echo "{$year} is leap Year";
+}else if($year % 4 == 0 && $year % 100 == 0){
+    echo "{$year} is not leap year";
+}else if($year % 4 == 0){
+    echo "{$year} is leap year";
+}else{
+    echo "{$year} is not leap year";
+}
+
+echo "\n";
+
+if($year % 4 == 0 && ($year % 100 != 0 || $year % 400 == 0)){
+    echo "{$year} is leap year";
+}else{
+    echo "{$year} is not leap year";
+}
